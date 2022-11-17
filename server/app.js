7db531721df19b276c402ffb1886c9f08755dfe6
fiskryeziu <fiskryeziu@gmail.com>
2022-11-17 18:50:20 +0100

    fix mp3 not converting

--- server/app.js
+++ server/app.js
@@ -3,6 +3,15 @@ import express from 'express'
 import cors from 'cors'
 import http from 'http'
 import { Server } from 'socket.io'
+import ffmpeg from 'fluent-ffmpeg'
+import ffmpeginstaller from '@ffmpeg-installer/ffmpeg'
+import fs from 'fs'
+
+const downloadFolder = process.env.USERPROFILE + '/Downloads'
+
+console.log(downloadFolder)
+const ffmpegPath = ffmpeginstaller.path
+ffmpeg.setFfmpegPath(ffmpegPath)
 
 const app = express()
 
@@ -22,27 +31,21 @@ const port = process.env.PORT || 5000
 
 let clientGlob = null
 
-const getAudio = (videoURL, res) => {
-  // console.log(videoURL)
-  // console.log(res)
-  let stream = ytdl(videoURL, {
-    quality: 'highestaudio',
-    filter: 'audioonly',
+const getAudio = async (videoURL, res) => {
+  const title = await ytdl.getInfo(videoURL).then((info) => {
+    return info.videoDetails.title
   })
-    .on('progress', (chunkSize, downloadedChunk, totalChunk) => {
-      clientGlob.emit('progressEventSocket', [
-        (downloadedChunk * 100) / totalChunk,
-      ])
-      clientGlob.emit('downloadCompletedServer', [downloadedChunk])
-      if (downloadedChunk == totalChunk) {
-        console.log('Downloaded')
-      }
-    })
-    .pipe(res)
+  let writeStream = fs.createWriteStream(`${downloadFolder}/${title}.mp3`)
 
-  ytdl.getInfo(videoURL).then((info) => {
-    clientGlob.emit('videoDetails', [info.videoDetails.title])
-  })
+  let stream = ytdl(videoURL, { filter: 'audioonly' })
+  ffmpeg(stream)
+    .audioCodec('libmp3lame')
+    .audioBitrate(128)
+    .format('mp3')
+    // .save(downloadFolder + title + '.mp3')
+    .on('error', (err) => console.error(err))
+    .on('end', () => console.log('Finished!'))
+    .pipe(writeStream)
 }
 
 app.post('/', (req, res) => {
